61c18e26cbaf93a1646754a2c02832b1ff95340a
Luiz Gustavo Oliveira Rocha <130007640+1LgR@users.noreply.github.com>
2025-02-28 11:46:23 +0000

    :heavy_plus_sign: feat: Adiciona while

--- produtor.py
+++ produtor.py
@@ -8,13 +8,19 @@ channel = connection.channel()
 # Declarar uma fila
 channel.queue_declare(queue='guessQueue')
 
-# Enviar mensagem para a fila
-message = 'Convidado recebe: 50'
-channel.basic_publish(exchange='',
-                      routing_key='guessQueue',
-                      body=message)
+num = 50
+cont = 1
 
-print(f"Mensagem enviada para a fila: {message}")
+# Enviar mensagem para a fila
+while(cont < 20):
+    message = (f"Convidado recebe: {num}")
+    num+=10
+    cont+=1
+    channel.basic_publish(exchange='',
+                        routing_key='guessQueue',
+                        body=message)
+
+    print(f"Mensagem enviada para a fila: {message}")
 
 # Fechar a conexão
 connection.close()
